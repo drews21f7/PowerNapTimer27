cf73b79423c72dd9f0c194ef9368563406e66def
Drew Seeholzer <drews21f7@gmail.com>
2019-06-18 10:52:20 -0600

    Views layed out

---
+++ PowerNapTimer27/View Controllers/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  PowerNapTimer27
+//
+//  Created by Drew Seeholzer on 6/18/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func updateLabel() {
+        
+    }
+    
+    func updateButton() {
+        
+    }
+    
+    func startTimer() {
+        
+    }
+    
+    func stopTimer() {
+        
+    }
+    
+    func setTimer() {
+        
+    }
+
+}
+
